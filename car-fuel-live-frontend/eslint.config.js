d8bf4a9419428a62a5c14361b7aa308be7f6d903
Jan Trautwein <jan.trautwein11@gmail.com>
2026-03-09 18:53:51 +0100

    Add new config files

---
+++ car-fuel-live-frontend/eslint.config.js
@@ -0,0 +1,28 @@
+﻿import pluginVue from "eslint-plugin-vue"
+import tseslint from "@typescript-eslint/eslint-plugin"
+import tsParser from "@typescript-eslint/parser"
+
+export default [
+  {
+    files: ["src/**/*.vue"],
+    languageOptions: {
+      parser: (await import("vue-eslint-parser")).default,
+      parserOptions: {
+        parser: tsParser
+      }
+    },
+    plugins: {
+      vue: pluginVue,
+      "@typescript-eslint": tseslint
+    },
+    rules: {
+      ...pluginVue.configs["flat/recommended"].rules
+    }
+  },
+  {
+    files: ["src/**/*.ts"],
+    languageOptions: { parser: tsParser },
+    plugins: { "@typescript-eslint": tseslint },
+    rules: { ...tseslint.configs["recommended"].rules }
+  }
+]
