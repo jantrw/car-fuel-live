dc54804be596be9e574fdf276e49da4e3ec1b14e
Jan Trautwein <114473304+jantrw@users.noreply.github.com>
2026-03-10 16:22:52 +0100

    Update gitignores and fix Tailwind version bug

--- car-fuel-live-frontend/tailwind.config.js
+++ car-fuel-live-frontend/tailwind.config.js
@@ -1,11 +1,12 @@
-﻿/** @type {import('tailwindcss').Config} */
-export default {
+﻿// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {}
+    extend: {},
   },
-  plugins: []
-}
+  plugins: [],
+};
